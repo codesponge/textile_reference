d9ef1ea62bb1928633c87e7f273dd468897bde43
codesponge <billy@codesponge.com>
2009-09-05 12:11:27 -0400

    fixed images and added quick

--- textile_reference.rb
+++ textile_reference.rb
@@ -1,13 +1,22 @@
 # textile_reference.rb
 require 'rubygems'
 require 'sinatra'
+
+THE_HOST_URL = 'http://textilereference.heroku.com'
 set :public, File.dirname(__FILE__) + '/static'
 
-get ( %r{(obake.jpg)|(openwindow1.jpg)|(sample.jpg)}i) do
-	
+#let the 3 images fall through to static
+get ( %r{(obake.gif)|(openwindow1.gif)|(sample.jpg)}i) do	
 end
 
+#the script's source
+get '/quick.html' do
+  @the_host_url = THE_HOST_URL
+  erb :quick
+end
 
+#make everything else give the reference!
 get '/*' do
+  @the_host_url = THE_HOST_URL
   erb :textile_reference
 end
